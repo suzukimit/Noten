6c53725972af9581feecc0163dd03f93c635c64f
Mitsuru Suzuki <mtr.suzuki@gmail.com>
2016-01-11 02:31:46 +0900

    Halfway1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    redirect_to :root if current_user != @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    redirect_to :root if current_user != @user
+
+    if @user.update(user_params)
+      flash[:success] = "Updated your profile!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
